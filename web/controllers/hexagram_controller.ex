d19858ab7a16d3df174b8b02d752819b0a2b6ca4
Michael Berkowitz <michael.berkowitz@gmail.com>
2016-01-26 08:21:48 -0500

    eex => slim

--- web/controllers/hexagram_controller.ex
+++ web/controllers/hexagram_controller.ex
@@ -5,6 +5,11 @@ defmodule IChing.HexagramController do
 
   def index(conn, _params) do
     hexagrams = Repo.all(from h in Hexagram, order_by: [asc: h.king_wen_number])
-    render conn, "index.html", hexagrams: hexagrams
+    render conn, hexagrams: hexagrams
+  end
+
+  def show(conn, %{"id" => id}) do
+    hexagram = Repo.get_by!(Hexagram, king_wen_number: id)
+    render conn, hexagram: hexagram
   end
 end
